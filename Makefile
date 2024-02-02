efd837e2a4a09b775c37561e9f574bef60473d83
Mourad Kejji <mkejji@gmail.com>
2024-02-03 00:13:48 +0100

    Simple start + txpool + Makefile

---
+++ Makefile
@@ -0,0 +1,45 @@
+include .env
+
+CONTAINER=
+DC=docker-compose
+
+.PHONY: up down stop ps re logs
+
+up:
+	$(DC) up -d
+
+down:
+	$(DC) down
+
+stop:
+	$(DC) stop
+
+ps:
+	$(DC) ps
+
+geth:
+	$(eval CONTAINER := geth)
+
+beacon:
+	$(eval CONTAINER := beacon_node)
+
+validator:
+	$(eval CONTAINER := validator_client)
+
+logs:
+	$(DC) logs --tail 100 -f --timestamps $(CONTAINER)
+
+bash:
+	$(DC) exec $(CONTAINER) bash
+
+restart:
+	$(DC) stop $(CONTAINER)
+	$(DC) up -d
+
+clean: down
+	sudo rm -rf geth-data/ geth-ancient-data/ jwttoken/ lighthouse-data/
+
+console:
+	$(DC) exec geth sh -c 'geth attach'
+
+re: down up
--- scripts/start-geth.sh
+++ scripts/start-geth.sh
@@ -2,18 +2,4 @@
 #
 # Starts a local fast-synced geth node.
 
-DEFAULT_NETWORK=mainnet
-
-if [ "$NETWORK" = "" ]; then
-	NETWORK=$DEFAULT_NETWORK
-fi
-
-if [ "$PRUNE_GETH" != "" ]; then
-    exec geth snapshot prune-state --datadir.ancient=/root/ancient-data
-elif [ "$START_GETH" != "" ]; then
-	if [ "$NETWORK" != "$DEFAULT_NETWORK" ]; then
-		exec geth --goerli --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --ipcdisable --authrpc.addr "0.0.0.0" --authrpc.port "8551" --authrpc.vhosts "*" --authrpc.jwtsecret "/root/jwttoken"
-	else
-		exec geth --syncmode snap --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --ipcdisable --datadir.ancient=/root/ancient-data --authrpc.addr "0.0.0.0" --authrpc.port "8551" --authrpc.vhosts "*" --authrpc.jwtsecret "/root/jwttoken/jwtsecret.hex"
-	fi
-fi
+geth --syncmode snap --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net,debug,txpool" --datadir.ancient=/root/ancient-data --authrpc.addr "0.0.0.0" --authrpc.port "8551" --authrpc.vhosts "*" --authrpc.jwtsecret "/root/jwttoken/jwtsecret.hex"
